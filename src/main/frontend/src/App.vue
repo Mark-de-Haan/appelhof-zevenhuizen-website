5b87c77786f2d980bff4304b45c56f83f46f5d0d
Mark-de-Haan <markdehaan90@gmail.com>
2018-06-04 23:20:36 +0200

    Fix site styling

--- src/main/frontend/src/App.vue
+++ src/main/frontend/src/App.vue
@@ -1,10 +1,11 @@
 <template>
-    <div class="container application-wrapper">
+    <div class="application-wrapper">
         <header>
             <navigation-bar/>
         </header>
 
         <main role="main">
+            <image-carousel/>
             <main-content/>
         </main>
 
@@ -15,15 +16,65 @@
     </div>
 </template>
 
-<script>
-  import MainContent from './components/MainContent'
-  import NavigationBar from './components/NavigationBar'
+<style>
+    /* GLOBAL STYLES
+-------------------------------------------------- */
+    /* Padding below the footer and lighter body text */
 
-  export default {
-    name: 'app',
-    components: {
-      MainContent,
-      NavigationBar
+    body {
+        padding-top: 3rem;
+        padding-bottom: 3rem;
+        color: #5a5a5a;
     }
-  }
+
+    /* Featurettes
+    ------------------------- */
+
+    .featurette-divider {
+        margin: 5rem 0; /* Space out the Bootstrap <hr> more */
+    }
+
+    /* Thin out the marketing headings */
+    .featurette-heading {
+        font-weight: 300;
+        line-height: 1;
+        letter-spacing: -.05rem;
+    }
+
+    /* RESPONSIVE CSS
+    -------------------------------------------------- */
+
+    @media (min-width: 40em) {
+        /* Bump up size of carousel content */
+        .carousel-caption p {
+            margin-bottom: 1.25rem;
+            font-size: 1.25rem;
+            line-height: 1.4;
+        }
+
+        .featurette-heading {
+            font-size: 50px;
+        }
+    }
+
+    @media (min-width: 62em) {
+        .featurette-heading {
+            margin-top: 7rem;
+        }
+    }
+</style>
+
+<script>
+    import ImageCarousel from './components/ImageCarousel'
+    import MainContent from './components/MainContent'
+    import NavigationBar from './components/NavigationBar'
+
+    export default {
+        name: 'app',
+        components: {
+            ImageCarousel,
+            MainContent,
+            NavigationBar
+        }
+    }
 </script>
