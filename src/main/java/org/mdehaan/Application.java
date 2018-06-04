10f26e603c20d5945ead754528fdde69040f307d
Mark-de-Haan <markdehaan90@gmail.com>
2018-06-04 22:10:55 +0200

    Remove unused annotations

--- src/main/java/org/mdehaan/Application.java
+++ src/main/java/org/mdehaan/Application.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguratio
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-@EnableWebMvc
-public class Application extends WebMvcAutoConfiguration
+public class Application
 {
     public static void main(String[] args)
     {
