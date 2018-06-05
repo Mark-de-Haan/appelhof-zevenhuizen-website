cc6b634920bd83119ea5892fc89769cb7035a64c
Mark-de-Haan <markdehaan90@gmail.com>
2018-06-05 20:23:08 +0200

    Replace Vue with thymeleaf fragments

--- src/main/java/org/mdehaan/controller/ApplicationController.java
+++ src/main/java/org/mdehaan/controller/ApplicationController.java
@@ -1,6 +1,5 @@
 package org.mdehaan.controller;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,10 +7,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ApplicationController
 {
-	@GetMapping("/")
-	public String index() {
-		return "index";
-	}
+    @GetMapping("/")
+    public String index(Model model)
+    {
+        model.addAttribute("active_menu", "index");
+        return "index";
+    }
 
-//	@GetMapping("/admin")
+    @GetMapping("/agenda")
+    public String agenda(Model model)
+    {
+        model.addAttribute("active_menu", "agenda");
+        return "agenda";
+    }
 }
